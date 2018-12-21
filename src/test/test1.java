35f93ac3f5827c01d68bed45b07f30eeb3491b55
Mr-zero8 <45451447+Mr-zero8@users.noreply.github.com>
2018-12-21 10:35:03 +0800

    delete
    delete

--- src/test/test1.java
+++ src/test/test1.java
@@ -2,7 +2,6 @@ package test;
 
 public class test1 {
 	public static void main(String[] args) {
-		System.out.println("主干master");
 		System.out.println("linghang的分支，等待合并");
 		System.out.println("在Git Hub上添加了这一句，如何updatae到Eclipse中去？");
 	}
