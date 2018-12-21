a70fa4366f66705633cf239a965ee7080aada212
Mr-zero8 <845966437@qq.com>
2018-12-21 10:58:21 +0800

    主干进行了修改

--- src/test/linghang.java
+++ src/test/linghang.java
@@ -2,6 +2,6 @@ package test;
 
 public class linghang {
 	public static void main(String[] args) {
-		System.out.println("linghang的JAVA，我修改了一下，准备合并了");
+		System.out.println("linghang的JAVA，我在主干上修改");
 	}
 }
